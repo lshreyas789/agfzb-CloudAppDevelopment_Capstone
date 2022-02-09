267e6e63b52752a2bd41b886bde0b6245457233c
lshreyas789 <shreshalakshman22@gmail.com>
2022-02-09 19:12:31 +0000

    updated

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -50,15 +50,19 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
+MIDDLEWARE_CLASSES = ( 
+
+    'django.middleware.common.CommonMiddleware', 
+
+    'django.contrib.sessions.middleware.SessionMiddleware', 
+
+    'django.middleware.csrf.CsrfViewMiddleware', 
+
+    'django.contrib.auth.middleware.AuthenticationMiddleware', 
+
+    'django.contrib.messages.middleware.MessageMiddleware', 
+
+) 
 
 ROOT_URLCONF = 'djangobackend.urls'
 
